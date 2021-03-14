da2657c9ef6292779bf47635bfd867393a0043b6
Abdimulhin2001 <adamabdimulhi.001@gmail.com>
2021-03-14 23:03:24 +0300

    added object to the variable

--- js/script.js
+++ js/script.js
@@ -9,13 +9,12 @@ function totalProduction() {
     var [a,b,c,d] = shades;
     var display = document.getElementById("display");
     var explanation = document.getElementById("explanation");
-    if(a.shade === 510 && b.shade === 308 && c.shade === 486 && d.shade === 572 ) {
+    if(a.value + b.value + c.value + d.value === 510 + 308 + 486 + 572) {
       display.innerHTML = "<p>The total PRODUCTION <span class='fill'> 1876 litres</span> per day</p>" 
     } else {
       a = parseFloat(a);
       b = parseFloat(b);
       c = parseFloat(c);
       d = parseFloat(d);
-      track(a,b,c,d);
     }
 }
