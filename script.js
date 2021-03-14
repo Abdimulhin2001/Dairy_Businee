69e3f561c4499abac957bc9158639678e92eae46
Abdimulhin2001 <adamabdimulhi.001@gmail.com>
2021-03-14 10:32:06 +0300

    added arrays to the  function

--- script.js
+++ script.js
@@ -6,11 +6,5 @@ function totalProduction() {
     shades.push(document.getElementById("shadeB").value);
     shades.push(document.getElementById("shadeC").value);
     shades.push(document.getElementById("shadeD").value);
-    //assign the values of the array to variables a,b,c
-    var [a,b,c,d] = shades;
-    var display = document.getElementById("display");
-    var explanation = document.getElementById("explanation");
-    if(a.shade === 0 || b.shade === 0 || c.shade === 0 || d.shade === 0 ) {
-      //check whether the user has typed something
-      display.innerHTML = "<p>Please <span class='fill'>FILL</span> all the fields</p>" }
-      console.log(display);
+    var [a,b,c,d]
+}
