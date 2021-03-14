bf3ca329bf4fc4c9f862e0357b78a205c0e4d487
Abdimulhin2001 <adamabdimulhi.001@gmail.com>
2021-03-14 12:56:02 +0300

    added else else function

--- script.js
+++ script.js
@@ -9,7 +9,19 @@ function totalProduction() {
     var [a,b,c,d] = shades;
     var display = document.getElementById("display");
     var explanation = document.getElementById("explanation");
-    if () {
-      
+    if ( a === 0, b === 0, c === 0, c === 0 ) {
+      display.innerHTML = "<p>Please <span class='fill'>FILL</span> all the fields</p>" 
+      } else {
+        //convert the strings to numbers
+        a = parseInt(a);
+        b = parseInt(b);
+        c = parseInt(c);
+        d = parseInt(d)
+        //track the triangle
+        track(a,b,c,d);
+      }
+      }
+    
     }
-}
+
+parseInt
