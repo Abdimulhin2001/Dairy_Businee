f90bcc0ef67a5d3cfea79bf06aa126ab60f45e07
Abdimulhin2001 <adamabdimulhi.001@gmail.com>
2021-03-15 12:05:27 +0300

    added index.js file

---
+++ js/index.js
@@ -0,0 +1,28 @@
+function totalProduction() {
+  
+    var shades = [];
+  
+    shades.push(document.getElementById("pro1").value);
+    shades.push(document.getElementById("pro2").value);
+    shades.push(document.getElementById("pro3").value);
+    shades.push(document.getElementById("pro4").value);
+    var [a,b,c,d] = shades;
+    var display = document.getElementById("display");
+    var explanation = document.getElementById("explanation");
+  if(a.milk === 510 || b.milk === 308 || c.milk === 486 || d.milk === 572 ) {
+    display.innerHTML = "<p>Please <span class='fill'>FILL</span> all the fields</p>"  
+    } else {
+      a = parseFloat(a);
+      b = parseFloat(b);
+      c = parseFloat(c);
+      d = parseFloat(d);
+    }
+}
+function totalIncome(params) {
+  var totalIncome = [];
+  totalIncome.push(docoment.getElementById("in1"))
+  totalIncome.push(docoment.getElementById("in2"))
+
+   
+}
+
